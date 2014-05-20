a7c8ac1ecca201cc270b1b8e36bab05a95b84c45
alexzam <alexzzam@gmail.com>
2014-05-20 17:09:22 +0400

    Init web application with Spring Boot.

---
+++ webapp/src/main/java/alexzam/vacation/controller/MainController.java
@@ -0,0 +1,13 @@
+package alexzam.vacation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String stub(){
+        return "main";
+    }
+}
